c798252eba6eff585464e260df476e5e52e83d16
Zizeko Anastasiya <nastjaznf@gmail.com>
2019-10-08 14:23:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+    config = { family: 4 }
+) {
+    if (knowsProgramming === true) {
+        preferences = Math.ceil(800 / config[focus]);
+    } else {
+        preferences = Math.ceil(1300 / config[focus]);
+    }
+    return preferences;
+};
